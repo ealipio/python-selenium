afaced3f38685c5f6a8d714d9499bbbdff2c1a29
Eisson Alipio <eisson.alipio@gmail.com>
2018-06-29 18:11:55 -0500

    Order in folder all the tests

--- test/my-first-test.py
+++ test/my-first-test.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome("./drivers/chromedriver.exe")
 browser.get('https://www.google.com/')
 browser.title
-elem = browser.find_element_by_id('lst-ib') # this is the input search box ID
+elem = browser.find_element_by_id('lst-ib')  # this is the input search box ID
 elem.send_keys('do a barrel roll' + Keys.RETURN)
-#elem.send_keys('#google gravity')
+# elem.send_keys('#google gravity')
 
-browser.quit() # close the browser
+browser.quit()  # close the browser
