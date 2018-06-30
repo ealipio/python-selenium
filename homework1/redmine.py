4a0e44ec9ea1b5884afe68fd435ab175376e6ae8
Eisson Alipio <eisson.alipio@gmail.com>
2018-06-30 00:51:27 -0500

    Using an assert to verify logging

--- homework1/redmine.py
+++ homework1/redmine.py
@@ -10,5 +10,8 @@ elem.send_keys(USER)
 elem = browser.find_element_by_id('password')
 elem.send_keys(PWD)
 elem.send_keys(Keys.RETURN)
-
+# browser.set_page_load_timeout(60)
+# verify if user is logged
+logged = browser.find_element_by_css_selector('div#loggedas')
+assert "Logged in as eisson" in logged.text
 browser.quit()  # close the browser
